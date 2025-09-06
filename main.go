81ac5df54b11a8e8655608d13a94d4ab31b88778
Xaitan <utrustad.sprang_0z@icloud.com>
2025-09-06 19:42:27 +0200

    coded for efficensy

--- main.go
+++ main.go
@@ -1,6 +1,40 @@
 package main
 
-func main() {
+import (
+    "io"
+    "os"
+)
 
-	println("I hope I get the job!")
+func main() {
+    f, err := os.Open("messages.txt")
+    if err != nil {
+        os.Stderr.Write([]byte("error: " + err.Error() + "\n"))
+        os.Exit(1)
+    }
+
+    buf := make([]byte, 8)
+    // Prebuild output buffer: "read: " + up to 8 bytes + "\n"
+    out := make([]byte, 6+8+1)
+    copy(out, "read: ")
+
+    for {
+        n, err := f.Read(buf)
+        if n > 0 {
+            copy(out[6:], buf[:n])
+            out[6+n] = '\n'
+            if _, werr := os.Stdout.Write(out[:6+n+1]); werr != nil {
+                os.Stderr.Write([]byte("error: " + werr.Error() + "\n"))
+                os.Exit(1)
+            }
+        }
+        if err == io.EOF {
+            break
+        }
+        if err != nil {
+            os.Stderr.Write([]byte("error: " + err.Error() + "\n"))
+            os.Exit(1)
+        }
+    }
+
+    _ = f.Close()
 }
